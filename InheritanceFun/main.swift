6ca38028f2b85e95b035ae11151096176bb7563c
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-09-18 11:01:02 -0700

    Initial Commit

---
+++ InheritanceFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InheritanceFun
+//
+//  Created by Crawford, Chloe on 9/18/20.
+//  Copyright © 2020 Crawford, Chloe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
